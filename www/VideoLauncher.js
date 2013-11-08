6ed9feb107ec8eecbdfad0d8d60212a7a990f22c
Tomáš Fejfar <tomas.fejfar@gmail.com>
2013-11-08 13:51:36 +0100

    The service is the name of plugin - not the actual class of the plugin

--- www/VideoLauncher.js
+++ www/VideoLauncher.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 module.exports = function(videoUrl, callback, errback) {
-    exec(callback, errback, 'W3WVideoLauncher', 'launch', [videoUrl]);
+    exec(callback, errback, 'VideoLauncher', 'launch', [videoUrl]);
 };
