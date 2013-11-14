e2947ffd1322e993e082f5141b9868bac93f8dab
Tomáš Fejfar <tomas.fejfar@gmail.com>
2013-11-14 15:42:52 +0100

    Plugin works even without callbacks specified

--- www/VideoLauncher.js
+++ www/VideoLauncher.js
@@ -1,5 +1,7 @@
 var exec = require('cordova/exec');
 
 module.exports = function(videoUrl, callback, errback) {
+    callback = callback || function(){};
+    errback = errback || function(){};
     exec(callback, errback, 'VideoLauncher', 'launch', [videoUrl]);
 };
